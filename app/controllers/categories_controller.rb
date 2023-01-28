3172114144d710b7498f13b7add70a8e80ab1e32
iliacodes <6ac177ed@protonmail.com>
2023-01-27 19:16:16 -0800

    adding controller and route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,15 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+
+  def new
   end
+
+  def index
+  end
+
+  def create
+  end
+  
 
 end
