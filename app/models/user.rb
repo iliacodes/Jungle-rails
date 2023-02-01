41193002b5b52e6cc39d74196959beacf8bba4ce
iliacodes <6ac177ed@protonmail.com>
2023-01-31 17:20:46 -0800

    added user specs feature for logging in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  before_save  :normalize_email
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true, length: {minimum: 2}
+  validates :last_name, presence: true, length: {minimum: 2}
+  validates :password, confirmation: true, presence: true, length: { minimum: 6}
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.strip)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
+  private
+  
+  def normalize_email
+    self.email = email.downcase.strip
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,105 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Validations" do
+    it "is valid" do
+      user = User.new(
+        first_name: "ilia",
+        last_name: "codes",
+        email: "iliacodes@github.com",
+        password: "1234567",
+        password_confirmation: "1234567"
+
+      )
+      expect(user).to be_valid
+    end
+  end
+
+  it "no email input" do
+    user = User.new(
+      first_name: "ilia",
+      last_name: "codes",
+      email: nil,
+      password: "1234567",
+      password_confirmation: "1234567"
+
+    )
+    expect(user).to be_invalid
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "no password input" do
+    user = User.new(
+      first_name: "ilia",
+      last_name: "codes",
+      email: "iliacodes@github.com",
+      password: nil,
+      password_confirmation: "1234567"
+
+    )
+    expect(user).to be_invalid
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "no confirmation password input" do
+    user = User.new(
+      first_name: "ilia",
+      last_name: "codes",
+      email: "iliacodes@github.com",
+      password: "1234567",
+      password_confirmation: nil
+
+    )
+    expect(user).to be_invalid
+    expect(user.errors[:password_confirmation]).to include("can't be blank")
+  end
+
+  it "no first name input" do
+    user = User.new(
+      first_name: nil,
+      last_name: "codes",
+      email: "iliacodes@github.com",
+      password: "1234567",
+      password_confirmation: "1234567"
+    )
+    expect(user).to be_invalid
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "no last name input" do
+    user = User.new(
+      first_name: "ilia",
+      last_name: nil,
+      email: "iliacodes@github.com",
+      password: "1234567",
+      password_confirmation: "1234567"
+    )
+    expect(user).to be_invalid
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  it "email with whitespaces input" do
+    user = User.new(
+      first_name: "ilia",
+      last_name: "codes",
+      email: " iliacodes@github.com   ",
+      password: "1234567",
+      password_confirmation: "1234567"
+    )
+    expect(user).to be_valid
+  end
+
+  it "email with caps input" do
+    user = User.new(
+      first_name: "ilia",
+      last_name: "codes",
+      email: " iliaCODES@github.com   ",
+      password: "1234567",
+      password_confirmation: "1234567"
+    )
+    expect(user).to be_valid
+  end
+
 end
