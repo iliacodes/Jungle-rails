83067703e990acaf0a35c03f6b620cb03561a2fe
iliacodes <6ac177ed@protonmail.com>
2023-01-31 19:05:08 -0800

    added feature spec: product details using home.spec as boilerplate. Ran home tests and fixed syntax errors.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,15 +1,15 @@
 describe('home page', () => {
   it('visits homepage', () => {
-    cy.visit('http://localhost:3000')
-  })
-});
+    cy.visit('http://localhost:3000');
+  });
 
-it("There is products on the page", () => {
-  cy.debug()
-  cy.get(".products article").should("be.visible")
-});
+  it("There is products on the page", () => {
+    cy.debug();
+    cy.get(".products article").should("be.visible");
+  });
 
-it("There is 2 products on the page", () => {
-  cy.debug()
-  cy.get(".products article").should("have.length", 2);
+  it("There is 2 products on the page", () => {
+    cy.debug();
+    cy.get(".products article").should("have.length", 12);
+  });
 });
